ccec82bba1302d42e65d4785b6675a09b6835708
Андрей Барышников <andrey1andrey@narod.ru>
2023-08-16 19:10:08 +0300

    5

--- src/main/java/ru/andreybaryshnikov/services/PhoneBookServiceImpl.java
+++ src/main/java/ru/andreybaryshnikov/services/PhoneBookServiceImpl.java
@@ -3,7 +3,6 @@ package ru.andreybaryshnikov.services;
 import ru.andreybaryshnikov.dao.PhonebookDAO;
 import ru.andreybaryshnikov.models.PhoneBook;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,32 +14,26 @@ public class PhoneBookServiceImpl implements PhoneBookService{
     }
 
     @Override
-    //@Transactional
     public PhoneBook getPhoneBook(int id) {
         return phoneBookDAO.getPhoneBook(id);
     }
     @Override
-   // @Transactional
     public List<PhoneBook> getPhoneBooks() {
         return phoneBookDAO.getPhoneBooks();
     }
     @Override
-    //@Transactional
     public void editRecordToPhoneBooks(PhoneBook newPhoneBook){
         phoneBookDAO.editRecordToPhoneBooks(newPhoneBook);
     }
     @Override
-    //@Transactional
     public void deleteRecordToPhoneBooks(int id){
         phoneBookDAO.deleteRecordToPhoneBooks(id);
     }
     @Override
-    //@Transactional
     public PhoneBook getNewPhoneBook() {
         return phoneBookDAO.getNewPhoneBook();
     }
     @Override
-    //@Transactional
     public void addRecordToPhoneBooks(PhoneBook newPhoneBook) {
         phoneBookDAO.addRecordToPhoneBooks(newPhoneBook);
     }
